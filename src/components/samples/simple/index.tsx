3b6a66ad20f22ef93d79b979602d2d87663ea59f
yoshida-san <yos.3104@gmail.com>
2020-01-13 23:33:27 +0900

    changed class component to functional component

--- src/components/samples/simple/index.tsx
+++ src/components/samples/simple/index.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.sass'
 
-// export default class Simple extends React.Component {
-//   public render() {
-//     return (
-//       <div>
-//         <h1>Hello Parcel + React !!</h1>
-//       </div>
-//     )
-//   }
-// }
-
+/**
+ * functional component
+ */
 const simpleComponent = () => {
   const element = <h1>simpleComponent !!</h1>
   return element
